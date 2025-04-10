8f986b60ca6bcbf7cebe8320c75c9da22ca62446
Efi Kay <efikay@icloud.com>
2025-04-10 09:39:41 +0300

    Move main.go to cmd/*

---
+++ cmd/debug/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi. There will be debug code (some sorts of dev testing etc)")
+}
--- cmd/tui/main.go
+++ cmd/tui/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Hello! TUI is not done yet")
 }
